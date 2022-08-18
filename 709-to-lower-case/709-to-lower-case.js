e4f19f315fc3d8bd5a4926dcc21126c1e5ece378
dev-rome <84602714+dev-rome@users.noreply.github.com>
2022-08-18 08:24:09 -0400

    Time: 98 ms (33.81%), Space: 42.1 MB (42.19%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.js
+++ 709-to-lower-case/709-to-lower-case.js
@@ -3,9 +3,10 @@
  * @return {string}
  */
 var toLowerCase = function(s) {
-    for(let i = 0; i < s.length; i++) {
-        if(s[i].toUpperCase()) {
-            return s.toLowerCase();
-        }
-    }
+    // for(let i = 0; i < s.length; i++) {
+    //     if(s[i].toUpperCase()) {
+    //         return s.toLowerCase();
+    //     }
+    // }
+    return s.toLowerCase()
 };
