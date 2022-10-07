020b2b49f7a76749ad6373ab78653884f3bbf7e8
dev-rome <84602714+dev-rome@users.noreply.github.com>
2022-10-07 16:23:57 -0400

    Time: 142 ms (37.24%), Space: 46.5 MB (18.92%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -5,7 +5,7 @@
 var buildArray = function(nums) {
     let arr = [];
     for(let i = 0; i < nums.length; i++) {
-        arr.push(nums[nums[i]])
+        arr[i] = nums[nums[i]]
     }
     return arr
 };
