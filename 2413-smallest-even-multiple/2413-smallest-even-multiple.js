c1a148e60a0eb7c4f878423a2697317cb89682d5
dev-rome <84602714+dev-rome@users.noreply.github.com>
2022-10-09 00:48:48 -0400

    Time: 106 ms (25.61%), Space: 41.8 MB (83.30%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+  let num = n * 2;
+  if (n % 2 === 0) {
+    return n;
+  } else {
+    return num;
+  }
+};
