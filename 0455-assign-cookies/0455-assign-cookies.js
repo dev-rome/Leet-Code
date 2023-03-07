69ca7f11a0e749321aba606726705b253f663fd4
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-03-07 15:04:50 -0500

    Time: 94 ms (75.23%), Space: 44.5 MB (69.55%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
-    s.sort((a,b) => a - b);
-    g.sort((a,b) => a - b);
-    
-    let count = 0;
-    let i = 0; 
-    let j = 0;
-    while(i < g.length && j < s.length ){
-      if(s[j] >= g[i]) {
-          count++
-          i++
-          j++
-      } else {
-          j++
-      }
+  s.sort((a, b) => a - b);
+  g.sort((a, b) => a - b);
+
+  let count = 0;
+  let i = 0;
+  let j = 0;
+  while (i < g.length && j < s.length) {
+    if (s[j] >= g[i]) {
+      count++;
+      i++;
+      j++;
+    } else {
+      j++;
     }
-    return count
+  }
+  return count;
 };
