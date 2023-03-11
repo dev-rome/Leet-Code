8ef3d762e93ddadba02d5eb012e1be64c4f6f02b
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-03-10 23:52:07 -0500

    Time: 62 ms (69.53%), Space: 42.3 MB (36.48%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -6,8 +6,7 @@
 var numOfStrings = function(patterns, word) {
   let count = 0;
   for (let i = 0; i < patterns.length; i++) {
-    let currentWord = patterns[i];
-    if (word.includes(currentWord)) count++;
+    if (word.includes(patterns[i])) count++;
   }
   return count;
 };
