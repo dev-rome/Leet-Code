991add8363e41b46940194682491f3cc201cac86
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-06-10 12:20:25 -0400

    Time: 71 ms (17.86%), Space: 42.9 MB (38.10%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -3,18 +3,32 @@
  * @return {number}
  */
 var findGCD = function(nums) {
+//   let min = nums[0];
+//   let max = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > max) max = nums[i];
+//     if (nums[i] < min) min = nums[i];
+//   }
+
+//   function helperGCD(a, b) {
+//     if (b === 0) return a
+//     let prime = a % b;
+//     return helperGCD(b, prime)
+//   }
+//   return helperGCD(min, max)
+    
+  let res = []
   let min = nums[0];
   let max = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > max) max = nums[i];
     if (nums[i] < min) min = nums[i];
   }
-
-  function helperGCD(a, b) {
-    if (b === 0) return a
-    let prime = a % b;
-    return helperGCD(b, prime)
+  for(let j = 1; j <= min; j++) {
+    if(min % j === 0 && max % j === 0) {
+      res.push(j);
+    }
   }
-  return helperGCD(min, max)
+  return Math.max(...res)
 };
     
