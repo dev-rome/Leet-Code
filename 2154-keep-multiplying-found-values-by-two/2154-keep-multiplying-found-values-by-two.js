794ad2be62698ed5e2b48962529e919b6df705c9
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-07-04 02:58:27 -0400

    Time: 61 ms (60.42%), Space: 43.2 MB (48.44%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -5,12 +5,11 @@
  */
 var findFinalValue = function(nums, original) {
   const obj = new Set(nums);
-  let sum = original;
 
   for (let i = 0; i < nums.length; i++) {
-    if (obj.has(sum)) {
-      sum = sum * 2;
+    if (obj.has(original)) {
+      original *= 2;
     }
   }
-  return sum;
+  return original;
 };
