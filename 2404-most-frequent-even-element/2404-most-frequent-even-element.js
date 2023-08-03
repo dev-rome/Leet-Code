2965ccd0fa660df3d7fcd7c573414dab9571f609
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-08-03 11:05:02 -0400

    Time: 63 ms (99.35%), Space: 49.2 MB (88.35%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var mostFrequentEven = function(nums) {
+    let map = new Map();
+	let element = Infinity;
+    let max = 0;
+
+	for (const num of nums) {
+		if(num % 2 == 0) {
+			let freq = 1;
+			
+			if(map.has(num))
+				freq = map.get(num) + 1;
+			
+			map.set(num, freq);
+
+			if(freq > max || freq == max && num < element){
+				element = num;
+				max = freq;
+			}
+		}
+	}
+	
+	return max == 0 ? -1 : element;
+};
