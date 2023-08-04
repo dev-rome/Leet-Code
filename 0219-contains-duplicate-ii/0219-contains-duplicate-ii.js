c12fc1de001630d4227da68d8157148e48427a6d
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-08-04 15:22:23 -0400

    Time: 80 ms (96.64%), Space: 67.2 MB (18.29%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,12 +4,13 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j] && Math.abs(i - j) <= k) {
-        return true;
-      }
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i - map.get(nums[i]) <= k) {
+      return true;
     }
+    map.set(nums[i], i);
   }
   return false;
 };
