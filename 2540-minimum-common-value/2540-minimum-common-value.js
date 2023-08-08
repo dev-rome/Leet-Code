1a414fd89c1aa9bcfb079bd52c96533cb94f6b71
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-08-08 00:12:58 -0400

    Time: 142 ms (12.28%), Space: 103 MB (5.26%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -6,16 +6,14 @@
 var getCommon = function(nums1, nums2) {
   const arr = [...new Set(nums1), ...new Set(nums2)];
   const map = {};
-  const max = Infinity;
+  let max = Infinity;
 
   for (const num of arr) {
     map[num] = (map[num] || 0) + 1;
-  }
 
-  for (const key in map) {
-    if (key < max && map[key] > 1) {
-      return key;
+    if (map[num] > 1 && num < max) {
+      max = num;
     }
   }
-    return -1;
+  return max === Infinity ? -1 : max;
 };
