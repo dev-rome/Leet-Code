accd5d5c31babd1877d6f1dee904b189fe9365f5
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-08-09 20:12:58 -0400

    Time: 142 ms (89.30%), Space: 50.6 MB (79.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,7 +6,7 @@ var isPalindrome = function(x) {
 let input = x.toString();
 let i = 0;
 let j = input.length - 1;
-while(j > 0) {
+while(j >= i) {
     if(input[i] !== input[j]) {
         return false
     }
