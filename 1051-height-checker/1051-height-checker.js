00e13b4b4849151af605e94de142a95660d6f659
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-08-13 14:00:55 -0400

    Time: 64 ms (31.20%), Space: 41.9 MB (76.50%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -4,8 +4,7 @@
  */
 var heightChecker = function(heights) {
     let count = 0;
-    let arr = heights.slice();
-    let expected = arr.sort((a, b) => a - b);
+    let expected = [...heights].sort((a, b) => a - b);
     
     for(let i = 0; i < heights.length; i++) {
         if(heights[i] !== expected[i]) count++
