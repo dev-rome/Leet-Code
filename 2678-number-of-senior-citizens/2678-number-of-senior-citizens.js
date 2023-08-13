d6f64da4f810c6cbac58a5cffa927b13208a7fa1
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-08-13 13:23:50 -0400

    Time: 40 ms (99.79%), Space: 43.9 MB (60.30%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.js
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var countSeniors = function(details) {
-      let count = 0;
+  let count = 0;
 
   for (let i = 0; i < details.length; i++) {
     let age = Number(details[i][11] + details[i][12]);
