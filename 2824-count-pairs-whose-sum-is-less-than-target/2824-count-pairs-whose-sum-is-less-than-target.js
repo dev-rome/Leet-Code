5feedc03d521cd86cecb4e2460814f0cdade46d7
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-08-19 19:03:56 -0400

    Time: 51 ms (100.00%), Space: 44.3 MB (100.00%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -8,7 +8,7 @@ var countPairs = function(nums, target) {
     
     for(let i = 0; i < nums.length; i++) {
         for(let j = i + 1; j < nums.length; j++) {
-            if(i < j && nums[i] + nums[j] < target) {
+            if(nums[i] + nums[j] < target) {
                 count++
             }
         }
