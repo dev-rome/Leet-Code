ec8e86b67ec3bcc6b9f734189f28d20a25e7533d
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-09-20 13:54:56 -0400

    Time: 51 ms (93.44%), Space: 44 MB (79.70%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-   let arr = [];
+    let arr = [];
     for(let i = 0; i < n; i++) {
         arr.push(nums[i]);
         arr.push(nums[i + n]);
