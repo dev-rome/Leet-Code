1880586fb3192f476d0d741fa5e5f13b5e5b94a4
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-09-30 20:03:59 -0400

    Time: 67 ms (72.47%), Space: 44.3 MB (57.74%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+   if(s.length !== t.length) return false;
+   let str1 = {};
+   let str2 = {};
+    
+    for(let val of s) {
+        str1[val] = (str1[val] || 0) + 1;
+    }
+    
+     for(let val of t) {
+         str2[val] = (str2[val] || 0) + 1;
+    }
+    
+    for(let key in str2) {
+        if(!(key in str1)) return false;
+        if(str2[key] !== str1[key]) return false;
+    }
+    
+    return true;
+};
