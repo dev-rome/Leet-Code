3c82b717f47b92fe556255e76c202a7ae40033f6
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-10-01 15:50:56 -0400

    Time: 62 ms (85.30%), Space: 44.3 MB (55.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,23 +3,18 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-   if(s.length !== t.length) return false;
-   let str1 = {};
-   let str2 = {};
-    
-    for(let val of s) {
-        str1[val] = (str1[val] || 0) + 1;
-    }
-    
-     for(let val of t) {
-         str2[val] = (str2[val] || 0) + 1;
-    }
-    
-    for(let key in str2) {
-        if(!(key in str1)) return false;
-        if(str2[key] !== str1[key]) return false;
-    }
-    
-    return true;
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let objLetter = {};
+
+  for (let c of s) {
+    objLetter[c] = (objLetter[c] || 0) + 1;
+  }
+
+  for (let c of t) {
+    if (!objLetter[c]) return false;
+    objLetter[c]--;
+  }
+
+  return true;
 };
