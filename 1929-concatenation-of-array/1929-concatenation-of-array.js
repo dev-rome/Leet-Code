b51a1dd6bc545e1ac0cb550b7e83dc0049e7a552
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-10-01 16:20:01 -0400

    Time: 65 ms (61.57%), Space: 45.3 MB (58.29%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,6 +3,10 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let arr = [...nums];
-    return arr.concat(nums);
+    let arr = [];
+    for(let i = 0; i < nums.length; i++) {
+        arr[i] = nums[i];
+        arr[i + nums.length] = nums[i];
+    }
+    return arr;
 };
