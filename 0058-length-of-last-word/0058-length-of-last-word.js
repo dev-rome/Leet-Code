9c69aa801ec6b5ec4b082f8fcdbea78f07e023e8
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-10-03 11:39:20 -0400

    Time: 41 ms (95.75%), Space: 42 MB (46.80%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,9 +3,21 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let removeSpace = s.trim();
-    let wordsArray = removeSpace.split(" ");
-    let lastWord = wordsArray[wordsArray.length - 1];
-    console.log(lastWord)
-    return lastWord.length;
+    // let removeSpace = s.trim();
+    // let wordsArray = removeSpace.split(" ");
+    // let lastWord = wordsArray[wordsArray.length - 1];
+    // return lastWord.length;
+  let lastWordLength = 0;
+  let firstChar = false;
+    
+  for (let i = s.length - 1; i >= 0; i--) {
+      if(firstChar & s[i] === " ") {
+          break;
+      }
+      if(s[i] !== " ") {
+          firstChar = true;
+          lastWordLength++;
+      }
+  }
+    return lastWordLength;
 };
