a17db3b74303e2cc6d5394201b55c2742aefbaf2
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-11-09 13:31:52 -0500

    Time: 56 ms (52.17%), Space: 41.8 MB (69.57%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
@@ -4,12 +4,14 @@
  */
 var maximumDifference = function(nums) {
     let maxValue = -1;
-   for(let i = 0; i < nums.length; i++) {
-       for(let j = 0; j < nums.length; j++) {
-           if(i < j && nums[i] < nums[j]) {
-               maxValue = Math.max(maxValue, nums[j] - nums[i])
-           }
-       }
-   }
+    let i = 0, j = 1;
+    while(j < nums.length) {
+        if(nums[i] < nums[j]) {
+            maxValue = Math.max(maxValue, nums[j] - nums[i]);
+        } else {
+            i = j;
+        }
+        j++;
+    }
     return maxValue;
 };
