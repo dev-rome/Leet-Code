cd9196c51a08017a5ea1bc4f1ea746b9413e3a78
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-11-17 17:39:34 -0500

    Time: 225 ms (69.78%), Space: 89.2 MB (20.52%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,37 +2,37 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {    
-    function mergeArray(left, right) {
+var sortArray = function(nums) {
+    if(nums.length <= 1) return nums;
+    const middle = Math.floor(nums.length / 2);
+    const leftHalf = nums.slice(0, middle);
+    const rightHalf = nums.slice(middle);
+    const sortLeft = sortArray(leftHalf)
+    const sortRight = sortArray(rightHalf);
+    return merge(sortLeft, sortRight)
+    
+    function merge(left, right) {
         let results = [];
         let i = 0;
         let j = 0;
         
         while(i < left.length && j < right.length) {
             if(left[i] < right[j]) {
-                results.push(left[i])
-                i++
+                results.push(left[i]);
+                i++;
             } else {
-                results.push(right[j])
-                j++
+                results.push(right[j]);
+                j++;
             }
         }
-        
         while(i < left.length) {
-            results.push(left[i])
-            i++
+            results.push(left[i]);
+            i++;
         }
-        
         while(j < right.length) {
-            results.push(right[j])
-            j++
+            results.push(right[j]);
+            j++;
         }
         return results;
     }
-    if(nums.length <= 1) return nums;
-    let mid = Math.floor(nums.length / 2);
-    let left = sortArray(nums.slice(0, mid));
-    let right = sortArray(nums.slice(mid));
-        
-    return mergeArray(left, right)
 };
