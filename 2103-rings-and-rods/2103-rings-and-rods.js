2aeeb9389fc79609efbcd9bdb732add16c60d386
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-11-18 01:49:27 -0500

    Time: 47 ms (86.85%), Space: 41.8 MB (78.87%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} rings
+ * @return {number}
+ */
+var countPoints = function(rings) {
+    let obj = new Map();
+    let count = 0;
+    let b = true;
+    let g = true;
+    let r = true;
+    
+    for(let i = 0; i < rings.length; i += 2) {
+        if(!obj.has(rings[i + 1])) {
+            obj.set(rings[i + 1], [rings[i]]);
+        } else {
+            obj.get(rings[i + 1]).push(rings[i]);
+        }
+    }
+    for(const [key, values] of obj.entries()) {
+        if(values.includes("B") && values.includes("R") && values.includes("G")) {
+            count++;
+        }
+    }
+    return count;
+};
