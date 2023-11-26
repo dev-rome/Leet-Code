67db1f18bcf0def91ad95d886231ccece60b8286
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-11-26 18:55:40 -0500

    Time: 56 ms (66.05%), Space: 42.3 MB (65.43%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -7,11 +7,7 @@ var differenceOfSums = function(n, m) {
     let sum1 = 0;
     let sum2 = 0;
     for(let i = 1; i <= n; i++) {
-        if(i % m !== 0) {
-            sum1 += i;
-        } else {
-            sum2 += i;
-        }
+        i % m !== 0 ? sum1 += i : sum2 += i;
     }
     return sum1 - sum2;
 };
