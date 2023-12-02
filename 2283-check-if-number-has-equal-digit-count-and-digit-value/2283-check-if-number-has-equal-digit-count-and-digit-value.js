391d9db67b0abfe445810083d8725cc550fb176b
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-12-02 18:14:44 -0500

    Time: 62 ms (23.48%), Space: 41.8 MB (90.91%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var digitCount = function(num) {
+    const n = num.length;
+    const count = new Array(10).fill(0);
+    for (let i = 0; i < n; i++) {
+        count[i] = Number(num[i]);
+    }
+    for (const c of num) {
+        count[c]--;
+    }
+    return count.every(v => v === 0);
+};
