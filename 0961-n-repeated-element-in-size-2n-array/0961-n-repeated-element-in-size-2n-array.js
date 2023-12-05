a06b03abdaa0c6853ab77244815e40ccabea8271
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-12-05 18:54:29 -0500

    Time: 72 ms (34.55%), Space: 50.1 MB (7.72%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+  const numMap = new Map();
+    for(const num of nums) {
+        if(numMap.has(num)) {
+            numMap.set(num, numMap.get(num) + 1);
+        } else {
+            numMap.set(num, 1);
+        }
+    }
+    for(const [key, value] of numMap.entries()) {
+        if(nums.length / 2 === value) return key;
+    }
+};
