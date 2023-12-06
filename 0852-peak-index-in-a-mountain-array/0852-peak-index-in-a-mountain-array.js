2758c3e95fb4587d3f09faacc59f7f7899822d64
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-12-05 21:31:23 -0500

    Time: 66 ms (45.21%), Space: 51.2 MB (61.06%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    let start = 0;
+    let end = arr.length - 1;
+    while(start < end) {
+        let mid = start + Math.floor((end - start) / 2);
+        if(arr[mid] < arr[mid + 1]) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+        return start;
+};
