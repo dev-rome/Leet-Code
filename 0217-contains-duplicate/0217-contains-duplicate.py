c231c5dd385454555d5c5c4814abeb5bad5ae732
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-12-19 21:33:10 -0500

    Time: 485 ms (6.50%), Space: 28.3 MB (67.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,13 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        dict = set()
-        for num in nums:
-            if num in dict:
+class Solution(object):
+    def containsDuplicate(self, nums):
+        freq_count = {}
+        for i in range(len(nums)):
+          freq_count[nums[i]] = freq_count.get(nums[i], 0) + 1
+        
+        for key in freq_count.keys():
+            if freq_count[key] > 1:
                 return True
-            dict.add(num)
-        return False
-       
             
+        return False
+        
+        
