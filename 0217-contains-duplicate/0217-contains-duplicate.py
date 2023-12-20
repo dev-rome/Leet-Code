97ab5cd422fef4716f3c8c3b55345ddcf448d5d2
dev-rome <84602714+dev-rome@users.noreply.github.com>
2023-12-19 21:36:07 -0500

    Time: 431 ms (48.41%), Space: 29.1 MB (28.39%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        freq_count = {}
-        for i in range(len(nums)):
-          freq_count[nums[i]] = freq_count.get(nums[i], 0) + 1
-        
-        for key in freq_count.keys():
-            if freq_count[key] > 1:
+        unique_set = set()
+        for n in nums:
+            if n in unique_set:
                 return True
-            
+            unique_set.add(n)
         return False
-        
-        
