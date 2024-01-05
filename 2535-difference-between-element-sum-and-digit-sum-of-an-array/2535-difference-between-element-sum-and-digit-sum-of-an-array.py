947923bda0f4dc9d652dec8e67b26bd1e8d3039f
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-01-05 13:09:54 -0500

    Time: 139 ms (23.35%), Space: 17.6 MB (14.45%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.py
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -3,7 +3,7 @@ class Solution:
         def digit_sum(num):
             return sum(int(digit) for digit in str(num) if digit.isdigit())
         
-        element_sum = sum(num for num in nums)
+        element_sum = sum(nums)
         digit_sum = sum(digit_sum(num) for num in nums)
         
         return abs(element_sum - digit_sum)
