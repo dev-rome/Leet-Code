abbc77578e6b9e836acd35a34c6737af13ca0ef7
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-01-10 11:40:39 -0500

    Time: 89 ms (97.49%), Space: 17.6 MB (28.00%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -4,10 +4,8 @@ class Solution:
         diagonal_sum = 0
 
         for i in range(n):
-            diagonal_sum += mat[i][i]  # Add elements from the primary diagonal
-
-            # Check if the element is not part of the primary diagonal
+            diagonal_sum += mat[i][i]
             if i != n - 1 - i:
-                diagonal_sum += mat[i][n - 1 - i]  # Add elements from the secondary diagonal
+                diagonal_sum += mat[i][n - 1 - i]
 
         return diagonal_sum
