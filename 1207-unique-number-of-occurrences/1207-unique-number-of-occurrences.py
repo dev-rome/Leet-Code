e32b56aba3fdc51ad04451a407987e825351fadb
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-01-16 22:20:25 -0500

    Time: 45 ms (48.62%), Space: 17.7 MB (9.52%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -2,10 +2,7 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         count_dict = {}
         for num in arr:
-            if num in count_dict:
-                count_dict[num] += 1
-            else:
-                count_dict[num] = 1
+            count_dict[num] = count_dict.get(num, 0) + 1
                 
         results = len(count_dict.values()) == len(set(count_dict.values()))
         
