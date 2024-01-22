ae4e58600d8b95c633f72471bda70e6916582aa8
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-01-22 10:22:47 -0500

    Time: 73 ms (55.23%), Space: 16.9 MB (49.84%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
@@ -3,6 +3,7 @@ class Solution:
         count = 0
         vowels = {"a", "e", "i", "o", "u"}
         for i in range(left, right + 1):
-            if words[i][0] in vowels and words[i][-1] in vowels:
+            word = words[i]
+            if word[0] in vowels and word[-1] in vowels:
                 count += 1
         return count
