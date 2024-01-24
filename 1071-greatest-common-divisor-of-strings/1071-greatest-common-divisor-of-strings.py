2a63a50ff2ceebfa18b810005a755c5e6b99d13c
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-01-24 17:15:02 -0500

    Time: 42 ms (41.82%), Space: 16.7 MB (59.39%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+        if (str1 + str2) != (str2 + str1):
+            return ""
+        
+        s1 = len(str1)
+        s2 = len(str2)
+        x = gcd(s1, s2)
+        return str1[:x]
+        
