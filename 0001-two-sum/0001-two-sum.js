1ee1ace3805980b777359d9fbab3be256ec2291f
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-02-18 21:59:17 -0500

    Time: 47 ms (97.51%), Space: 50.3 MB (26.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  const obj = {};
-  for (let i = 0; i < nums.length; i++) {
-    let output = target - nums[i];
-    if (output in obj) {
-      return [obj[output], i];
+    const numsObj = {}
+    for(let i = 0; i < nums.length; i++) {
+        const output = target - nums[i];
+        if(output in numsObj) {
+            return [numsObj[output], i]
+        }
+        numsObj[nums[i]] = i
     }
-    obj[nums[i]] = i;
-  }
+    
 };
